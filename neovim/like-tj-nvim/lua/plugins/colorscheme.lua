401384cb31ab99d924d90bce4095c3f70b67eb30
dashabi99 <shudaixiong081@gmail.com>
2025-11-18 17:38:28 +0800

    update color

--- neovim/like-tj-nvim/lua/plugins/colorscheme.lua
+++ neovim/like-tj-nvim/lua/plugins/colorscheme.lua
@@ -1,21 +1,16 @@
 -- 如果是使用的lazyvim用上面的，如果使用的自己配置的用下面的
 return {
-  -- add gruvbox
   { "tjdevries/colorbuddy.nvim" },
-
-  -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "gruvbuddy",
-      -- Shatur/neovim-ayu
     },
   },
 }
 -- return {
 --   lazy = false,
 --   priority = 1000,
---   -- dir = "~/plugins/colorbuddy.nvim",
 --   "tjdevries/colorbuddy.nvim",
 --   config = function()
 --     vim.cmd.colorscheme("gruvbuddy")
